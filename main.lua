4f8aa2928e62b6f2e7ff08d3cf334107a124705c
Nihaal Sangha <nihaal.git@gmail.com>
2026-03-18 12:06:14 +0000

    Remove update checker

--- main.lua
+++ main.lua
@@ -4,15 +4,11 @@ local NetworkMgr = require("ui/network/manager")
 local _ = require("gettext")
 
 local showChatGPTDialog = require("dialogs")
-local UpdateChecker = require("update_checker")
 
 local AskGPT = InputContainer:new {
   name = "askgpt",
   is_doc_only = true,
 }
-
--- Flag to ensure the update message is shown only once per session
-local updateMessageShown = false
 
 function AskGPT:init()
   self.ui.highlight:addToHighlightDialog("askgpt_ChatGPT", function(_reader_highlight_instance)
@@ -21,10 +17,6 @@ function AskGPT:init()
       enabled = Device:hasClipboard(),
       callback = function()
         NetworkMgr:runWhenOnline(function()
-          if not updateMessageShown then
-            UpdateChecker.checkForUpdates()
-            updateMessageShown = true -- Set flag to true so it won't show again
-          end
           showChatGPTDialog(self.ui, _reader_highlight_instance.selected_text.text)
         end)
       end,
